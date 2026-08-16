13971e484685734226eb9f0ab2d718f0d27db28d
subash pradhan <subashpradhan96926@gmail.com>
2026-08-16 20:07:08 +0530

    15.08,26

---
+++ plsql/concatenation.sql
@@ -0,0 +1,7 @@
+declare
+    first_name varchar(20):='subash';
+    last_name varchar(20):='pradhan';
+begin
+	dbms_output.put_line(first_name||''||last_name);
+end;
+/
---
+++ plsql/operater.sql
@@ -0,0 +1,11 @@
+declare 
+    a number:=10;
+    b number:=3;
+begin
+	dbms_output.put_line(a+b);
+	dbms_output.put_line(a-b);
+	dbms_output.put_line(a*b);
+	dbms_output.put_line(a/b);
+	dbms_output.put_line(a**2);
+end;
+/
